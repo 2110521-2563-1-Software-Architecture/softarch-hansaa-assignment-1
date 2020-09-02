f11777ac5c0d3dea206c4181426330c2c23956ad
Prawsang <prawcha.p@gmail.com>
2020-09-02 21:32:52 +0700

    rest

---
+++ rest/models/book.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  borrowed: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      book:
+ *        type: object
+ *        required:
+ *          - name
+ *          - author
+ *          - borrowed
+ *        properties:
+ *          name:
+ *            type: string
+ *          author:
+ *            type: string
+ *          borrowed:
+ *            type: boolean
+ *        example:
+ *           name: BookName
+ *           author: AuthorName
+ *           borrowed: false
+ */
---
+++ rest/routes/books.js
@@ -0,0 +1,228 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const Book = require("../models/Book");
+
+/**
+ * @swagger
+ * /books:
+ *    get:
+ *     description: Get all books
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ errors: err });
+  }
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *    get:
+ *     description: Get a book by ID
+ *     parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID of the book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+router.get("/:id", async (req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => {
+      res.status(200).json(book);
+    })
+    .catch((err) => {
+      res.status(400).json({ errors: err });
+    });
+});
+
+/**
+ * @swagger
+ * /books:
+ *    post:
+ *      description: Create a book
+ *      parameters:
+ *        - name: book
+ *          in: body
+ *          description: Name of our book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *        - name: author
+ *          in: body
+ *          description: Author of our book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *      responses:
+ *        '201':
+ *          description: Success
+ */
+router.post(
+  "/",
+  [body("name").notEmpty(), body("author").notEmpty()],
+  async (req, res) => {
+    const book = new Book({
+      name: req.body.name,
+      author: req.body.author,
+      borrowed: false,
+    });
+
+    try {
+      const newBook = await book.save();
+      res.status(201).json(newBook);
+    } catch (err) {
+      res.status(400).json({ errors: err });
+    }
+  }
+);
+
+/**
+ * @swagger
+ * /books/{id}:
+ *    put:
+ *      description: Edit a book
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID of the book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *        - name: book
+ *          description: Name of our book
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *        - name: author
+ *          description: Author of our book
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.put(
+  "/:id",
+  [body("name").notEmpty(), body("author").notEmpty()],
+  async (req, res) => {
+    try {
+      await Book.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        author: req.body.author,
+      });
+      res.status(200).json(true);
+    } catch (err) {
+      res.status(400).json({ errors: err });
+    }
+  }
+);
+
+/**
+ * @swagger
+ * /books/{id}/borrow:
+ *    put:
+ *      description: Borrow a book
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID of the book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.put("/:id/borrow", async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  if (!book.borrowed) {
+    try {
+      await Book.findByIdAndUpdate(req.params.id, {
+        borrowed: true,
+      });
+      res.status(200).json(true);
+    } catch (err) {
+      res.status(400).json({ errors: err });
+    }
+  } else {
+    res.status(400).json({ errors: { message: "Book is not available." } });
+  }
+});
+
+/**
+ * @swagger
+ * /books/{id}/return:
+ *    put:
+ *      description: Return a book
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID of the book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.put("/:id/return", async (req, res) => {
+  try {
+    await Book.findByIdAndUpdate(req.params.id, {
+      borrowed: false,
+    });
+    res.status(200).json(true);
+  } catch (err) {
+    res.status(400).json({ errors: err });
+  }
+});
+
+/**
+ * @swagger
+ * /books/{id}:
+ *    delete:
+ *     description: Delete a book by ID
+ *     parameters:
+ *        - name: id
+ *          in: path
+ *          description: ID of the book
+ *          required: true
+ *          schema:
+ *            type: string
+ *            format: string
+ *     responses:
+ *       '200':
+ *         description: Success
+ */
+router.delete("/:id", async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.status(200).json(true);
+  } catch (err) {
+    res.status(400).json({ errors: err });
+  }
+});
+
+module.exports = router;
